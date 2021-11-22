d19bf7d2ab9a755201eda10ddb5b74847b473d20
40010753 <bharath.g@ltts.com>
2021-11-22 10:25:11 +0530

    Added Dummy function

--- Multifile/feature.c
+++ Multifile/feature.c
@@ -12,6 +12,18 @@ int subtract(int num1, int num2)
     return num1 - num2;
 }
 
+int sum ;
+int* divide(int numerator, int denominator )
+{
+    if(0 == denominator)
+    {
+
+    }
+    int* var;
+
+    return var;
+}
+
 /*
     Unit testing
 */
