c7b9cede5edb6fade8d74bfdc11e2328e227210b
40010753 <bharath.g@ltts.com>
2021-11-22 09:38:47 +0530

    Added Multifile program

---
+++ Multifile/feature.c
@@ -0,0 +1,17 @@
+#include "feature.h"
+/*
+    Avoid printf and scanf functions
+*/
+int addition_funtion(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+int subtract(int num1, int num2)
+{
+    return num1 - num2;
+}
+
+/*
+    Unit testing
+*/
