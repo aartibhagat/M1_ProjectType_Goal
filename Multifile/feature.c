5ede61167ce6a15e6a6c54f84b69bdf32eb5a5a0
40010753 <bharath.g@ltts.com>
2021-11-23 09:47:06 +0530

    Adding Pointers examples, #1

--- Multifile/feature.c
+++ Multifile/feature.c
@@ -27,3 +27,34 @@ int* divide(int numerator, int denominator )
 /*
     Unit testing
 */
+
+#include "stdio.h"
+int main() {
+
+  int A = 10;
+  // Value, Address, Memory
+  printf("Value of A = %d, Address of A is %p\n", &A);
+  
+  int Array[10] = {1,2,3,4,5,6,7,8,9,10};
+  printf("Value of Array[5] = %d\n", Array[5]);
+  
+  int B = &A;
+  printf("Value of B = %d\n", B);
+  
+  int* ptr = &A;
+
+float val = 10.3;
+float* ptr_f0 = &val;
+
+    int val1 = 0x40414243;
+   
+    char* ptr_c = &val;
+    int* ptr_i = &val;
+    float* ptr_f = &val;
+
+    //print *ptr_c - 1 bytes from Memory
+
+  printf("Value of ptr = %d, Value at ptr = %d\n", ptr, *ptr);
+  
+  return 0;
+}
